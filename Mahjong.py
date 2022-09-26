889ca9f9b78c1024c3b97cd67e6f07351bc17536
Geusen <89836078+Geusen@users.noreply.github.com>
2022-09-26 21:20:29 +0900

    finished

--- Mahjong.py
+++ Mahjong.py
@@ -4,7 +4,7 @@ import tweepy
 import random
 from PIL import Image
 
-'''
+
 #-----------------------------------------------------------------------------
 # keyの指定(情報漏洩を防ぐため伏せています)
 consumer_key = settings.CK
@@ -20,44 +20,64 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #-----------------------------------------------------------------------------
-'''
-def oya():
+# 牌決め
+def haipai():
+    # 萬子、筒子、索子、字牌決め
     tile1 = chr(random.randint(65, 68))
+    # 字牌の場合は7種類のどれか
     if tile1 == 'D':
         tile2 = str(random.randrange(1, 7))
     else:
         tile2 = str(random.randrange(1, 9))
-        if tile2 == '5' and random.randint(0, 1) == 1 and l.count(tile1 + '6!') == 0:
+        # 赤ドラ抽選(既にある場合は除外)
+        if tile2 == '5' and random.randint(0, 4) == 1 and l.count(tile1 + '6!') == 0:
             tile2 = '6!'
     global tile
     tile = tile1 + tile2 + '.png'
 
+# 廃の画像貼り付け
+def image_paste(a, b, c):
+    # 背景画像、牌の画像を開く
+    img1 = Image.open('./images/upload.png')
+    img2 = Image.open('./images/' + a)
+    # サイズ調節
+    img2_size = (round(img2.width * 0.25), round(img2.height * 0.25))
+    img2 = img2.resize(img2_size)
+    # 背景と同サイズの透明な画像を生成し、透明画像の上に牌の画像をペースト
+    img_clear = Image.new("RGBA", img1.size, (255, 255, 255, 0))
+    img_clear.paste(img2, (b, c))
+    img1 = Image.alpha_composite(img1, img_clear)
+    img1.save('./images/upload.png', quality=95)
 
+
+#-----------------------------------------------------------------------------
 l = []
 num = 0
+# 牌の数が14になるまで繰り返す
 while num < 14:
-    oya()
+    haipai()
+    # 同じ牌が既に4つあるなら除外
     if l.count(tile) < 4:
         l.append(tile)
         num += 1
 print(sorted(l))
+# ドラ表示牌抽選
 while num < 15:
-    oya()
+    haipai()
     if l.count(tile) < 4:
         dora = tile
         num += 1
-print(dora)
 
+# 牌の画像並べ
 back = Image.open('./images/background.png')
 img1 = back.copy()
-x = 0
+x = 148
 img1.save('./images/upload.png', quality=95)
-for i in l:
-    img1 = Image.open('./images/upload.png')
-    img2 = Image.open('./images/' + i)
-    size = (round(img2.width * 0.25), round(img2.height * 0.25))
-    img2 = img2.resize(size)
-    img1.paste(img2, (x, 50))
-    img1.save('./images/upload.png', quality=95)
-    x += 60
-    
+for i in sorted(l):
+    image_paste(i, x, 393)
+    x += 65
+image_paste(dora, 570, 188)
+
+#-----------------------------------------------------------------------------
+# ツイート
+api.update_status_with_media(filename="./images/upload.png")
